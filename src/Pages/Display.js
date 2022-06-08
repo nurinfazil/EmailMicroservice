3dfc40188e03464b9ac03ae6d94d2c54d600daa6
Nurin Fazil <nurinfazil@hotmail.com>
2022-06-08 13:10:16 -0400

    CORS stuff fixed

--- src/Pages/Display.js
+++ src/Pages/Display.js
@@ -15,19 +15,20 @@ const Display = () => {
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
                 // 'Content-Type': 'application/x-www-form-urlencoded',
             },
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         })
-            .then(response => console.log(response.body))
+            .then(response => console.log(response.text()))
             .then(data => {
                 // console.log(data)
             });
 
         console.log(id)
-    }, [id])
+    }, [])
 
     return (
         <div className="display-account">
